7cc09154d7098c678ff65b9fe08e71af1594e845
Kseniya Kunda <kseniya_kunda@epam.com>
2017-07-17 17:03:01 +0300

    first commit

---
+++ src/test/java/TestMaven.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Kseniya_Kunda on 7/17/2017.
+ */
+public class TestMaven {
+
+
+
+}
---
+++ src/test/java/TestTestNG.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Kseniya_Kunda on 7/17/2017.
+ */
+public class TestTestNG {
+
+}
