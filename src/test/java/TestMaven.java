8dc6e9957f0e4e9cb19a9dd6a403ea2058b57d75
Kseniya Kunda <kseniya_kunda@epam.com>
2017-07-17 17:07:25 +0300

    add maven test

--- src/test/java/TestMaven.java
+++ src/test/java/TestMaven.java
@@ -6,6 +6,14 @@ import org.testng.annotations.Test;
  */
 public class TestMaven {
 
+    private int a = 5;
+    private int b = 3;
+    private int sum = 8;
 
+    @Test
+    public void simpleTest() {
+        int result = a + b;
+        Assert.assertTrue(result == sum, "Sum is not correct");
+    }
 
 }
